dbf8b07c3c4f1e7bcf2a168c8cbc955a28463795
prirya <prirya2@gmail.com>
2021-01-22 19:24:34 +0700

    Add project files.

---
+++ bookStoreApp/DataAccess.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace bookStoreApp
+{
+    class DataAccess
+    {
+
+        public const string dbpath = "Database_BookStore.db";
+        private static object createTable;
+
+        public static void InitializeDatabase()
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                String tableCommand =
+                    //ตารางเก็บข้อมูลลูกค้า
+                    "CREATE TABLE IF NOT EXISTS CustomersTable (`Customer ID` INTEGER PRIMARY KEY, `Name` NVARCHAR(50) NULL, `Address` NVARCHAR(500) NULL, `Email` NVARCHAR(50) NULL);" +
+                    //สร้างตารางหนังสือ
+                    "CREATE TABLE IF NOT EXISTS BookTable (`ISBN` CHAR(10) PRIMARY KEY, Title NVARCHAR(200) NULL,Description NVARCHAR(500) NULL,Price DOUBLE NULL);" +
+                    //ข้อมูลการขาย
+                    "CREATE TABLE IF NOT EXISTS Transactions (`ISBN` CHAR(10) PRIMARY KEY,`Customers ID` NVARCHAR(200) NULL,Quatity INTEGER NULL,`Total Price` DOUBLE NULL);";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void AddDataCustomerTable(string name, string address, string email)
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO CustomersTable VALUES (NULL,@Name,@Address,@Email);";
+                insertCommand.Parameters.AddWithValue("@Name", name);
+                insertCommand.Parameters.AddWithValue("@Address", address);
+                insertCommand.Parameters.AddWithValue("@Email", email);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+        }
+        public static void AddDataBookTable(string isbn, string title, string description, string price)
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO BookTable VALUES(@isbn,@title,@description,@price);";
+                insertCommand.Parameters.AddWithValue("@isbn", isbn);
+                insertCommand.Parameters.AddWithValue("@title", title);
+                insertCommand.Parameters.AddWithValue("@description", description);
+                insertCommand.Parameters.AddWithValue("@price", price);
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+        }
+        public static void AddDataTransactions(string isbn, string customersID, string quatity, string totalPrice)
+        {
+        
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO BookTable VALUES(@isbn,`@Customers ID`,@quatity,`@Total Price`);";
+                insertCommand.Parameters.AddWithValue("@isbn", isbn);
+                insertCommand.Parameters.AddWithValue("@title", customersID);
+                insertCommand.Parameters.AddWithValue("@description", quatity);
+                insertCommand.Parameters.AddWithValue("@price", totalPrice);
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+        }
+        public static List<String> GetData() //คือการแสดงผลข้อมูลที่อยู่ใน dataBase ออกมาทั้งหมด
+        {
+            List<String> entries = new List<string>();
+
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT Name,Address,Email from CustomersTable", db); //หลัง SELECT คือใส่ชื่อ field หากมีหลาย field ข้องใส่ (,) เอาไว้ด้วย แต่ถ้าอยากได้ทั้งหมดเลยก็ใส่ (*)
+
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add($"{query.GetString(0)} {query.GetString(1)} ({query.GetString(2)})"); //มันใช้งานเหมือน ArrayList คือกำหนดให้ส่งค่ากลับออกมาเป็นแต่ละ column (field) โดยเรียงตามที่เราเรียง Opject [0][1][2]
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+
+
+    }
+}
---
+++ bookStoreApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bookStoreApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+        }
+
+        private void adddatabottom_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddDataCustomerTable(nameTextbox.Text, addressTextbox.Text, emailTextbox.Text);
+            nameTextbox.Text = addressTextbox.Text = emailTextbox.Text = "";
+        }
+
+        private void showBottom_Click(object sender, RoutedEventArgs e)
+        {
+                string textResult = ""; //ข้อความว่างสำหรับเก็บ Entry อื่นๆ
+                List<string> getData = DataAccess.GetData(); //เอาข้อมูลจาก database มาเก็บเป็นตัวแปล getdata
+                for (int i = 0; i < getData.Count; i++) //วนซ้ำตามเงื่อนไข
+                {
+                    textResult += getData[i] + "\n"; //ขึ้นบรรทัดใหม่ // += คือ ต่อเติมค่าที่อยู่ข้างหน้าเข้าไปหาค่าที่อยู่ข้างหลัง ยกตัวอย่าง A += B ก็จะได้ AB
+                }
+                MessageBox.Show(textResult);
+        }
+    }
+}
